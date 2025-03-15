95ff869523f113114f6a9886245c4e5f35bc30b7
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-03-15 23:29:08 +0530

    initial commit

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,1 +1,27 @@
-const User = require('../model/user')
+const User = require('../model/user');
+const bcrypt = require('bcrypt');
+
+const signup = async (req, res) => {
+  const { userName, userEmail, userPassword } = req.body;
+  try {
+    if (!userName || !userEmail || !userPassword) {
+      return res.status(400).json({ message: 'Missing required fields' });
+    }
+
+    const existingUser = await User.findOne({ userEmail });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'User email already exists!' });
+    } else {
+      const hashedPassword = await bcrypt.hash(userPassword, 10);
+      const user = new User({ userName, userEmail, userPassword: hashedPassword });
+      await user.save();
+      res.status(201).json({ message: 'User signup successfully' });
+    }
+  } catch (error) {
+    console.error('Signup error:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+module.exports = signup;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const connectDB = require('./config/db.js') 
+const routes = require('./routes/userRoutes.js')
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
 app.use(express.json())
+app.use('/api',routes);
 
 connectDB();
+
 app.listen(PORT,()=>{
     console.log(`server running on localhost:${process.env.PORT}`);
 })
